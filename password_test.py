4143137a57c67c4dd1ddbdcd62c88c5b09fd6356
Sunny Choi <yukhengchoi@gmail.com>
2020-04-26 03:30:58 +0800

    shorten the code

--- password_test.py
+++ password_test.py
@@ -2,11 +2,14 @@ password = 'a123456'
 chance = '3'
 chance = int(chance)
 while chance > 0:
+	chance = chance - 1
 	pw = input('Please enter the password: ')
 	if password == pw:
 		print('Login success!')
 		break
 	else:
-		chance = chance - 1
-		print('Wrong password, you have', chance, 'more chances!')
-		
+		print('Wrong password!')
+		if chance > 0:
+			print('You have', chance, 'more chances!')
+		else:
+			print('Your account would be locked in the coming 24 hours!')
